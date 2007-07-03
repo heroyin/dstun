a798963da5d5c12d35611caff033829a4c778c15
heroyin <heroyin@034b90dd-5134-0410-b380-e5a2608eef86>
2007-07-03 01:54:10 +0000

    Added a folder remotely

---
+++ Dstun/DStuntest.dpr
@@ -0,0 +1,14 @@
+program DStuntest;
+
+uses
+  Forms,
+  TestMain in 'TestMain.pas' {Form1},
+  DStun in 'src\DStun.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
